e34d55b3e75417c2effc82571673e3a89cbf3bc7
Hans Li <hans@Hanss-MBP.etolli.com>
2018-11-11 21:27:33 -0800

    Add typescript loader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './src/index',
 
+  devtool: 'inline-source-map',
+
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -13,6 +15,16 @@ module.exports = {
   },
 
   module: {
-    rules: [{ test: /\.(ts|js)x?$/, loader: 'babel-loader', exclude: /node_modules/ }],
+    rules: [
+      { 
+        test: /\.(js)x?$/,
+        loader: 'babel-loader',
+         exclude: /node_modules/
+      },
+      {
+        test: /\.(ts)x?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }],
   },
 };
